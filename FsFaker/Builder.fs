f0083cb9ad7f3985054fe155343e040ca161995c
Lucas Teles <lucas.teles@outlook.com>
2024-01-09 17:11:52 -0300

    WIP

--- FsFaker/Builder.fs
+++ FsFaker/Builder.fs
@@ -1,4 +1,77 @@
 ﻿namespace FsFaker.Builder
 
 open Bogus
+open FsFaker
 
+type DataBuilder<'t> =
+    { Constructor: Faker -> 't
+      Faker: Faker
+      Mapper: 't -> 't }
+
+    member this.Generate() =
+        this.Faker |> this.Constructor |> this.Mapper
+
+    member this.Generate(count: int) =
+        [ for _ = 0 to count do
+              this.Generate() ]
+
+    member this.GenerateInfinite() =
+        Seq.initInfinite (fun _ -> this.Generate())
+
+// member this.With(field: Expression<Func<'t, 'u>>, value: 'u) =
+//     let memberInfo = Helpers.getMemberInfo field
+//     // let ctor = FSharpValue.cons
+//     this
+
+[<RequireQualifiedAccess>]
+module Builder =
+
+    let create<'t> ctor : DataBuilder<'t> =
+        { Constructor = ctor
+          Faker = FsFakerConfig.newDataFaker ()
+          Mapper = id }
+
+    let generate (b: DataBuilder<_>) = b.Generate()
+    let infinite (b: DataBuilder<_>) : 't seq = b.GenerateInfinite()
+
+    let one (b: DataBuilder<_>) = generate b
+    let two (b: DataBuilder<_>) = one b, one b
+    let three (b: DataBuilder<_>) = one b, one b, one b
+
+    let between initValue endValue (f: DataBuilder<_>) =
+        let count = f.Faker.Random.Int(min = initValue, max = endValue - 1)
+        f.Generate count
+
+    let list (count: int) (b: DataBuilder<_>) = b.Generate count
+    let seq (count: int) (b: DataBuilder<_>) = b.GenerateInfinite() |> Seq.take count
+    let array (count: int) (b: DataBuilder<_>) = b |> list count |> List.toArray
+
+    let setLocale (locale: string) (b: DataBuilder<_>) = b.Faker.Locale <- locale
+
+    let seed (localSeed: int) (b: DataBuilder<_>) = b.Faker.Random <- Randomizer localSeed
+
+    let update f (b: DataBuilder<_>) =
+        { b with
+            Mapper = b.Mapper >> f b.Faker }
+
+    let combine (a: DataBuilder<'t>) (b: DataBuilder<'t>) =
+        { b with Mapper = b.Mapper >> a.Mapper }
+
+    let map f (b: DataBuilder<_>) = { b with Mapper = b.Mapper >> f }
+
+    let zip (a: DataBuilder<'a>) (b: DataBuilder<'b>) =
+        { Constructor = fun _ -> generate a, generate b
+          Faker = b.Faker
+          Mapper = id }
+
+    let zip3 (a: DataBuilder<'a>) (b: DataBuilder<'b>) (c: DataBuilder<'c>) =
+        { Constructor = fun _ -> generate a, generate b, generate c
+          Faker = b.Faker
+          Mapper = id }
+
+    let from (value: 'a) : DataBuilder<'a> = create (fun _ -> value)
+
+    let bind (f: 'a -> DataBuilder<'b>) (b: DataBuilder<'a>) : DataBuilder<'b> =
+        { Constructor = fun _ -> b |> generate |> f |> generate
+          Faker = b.Faker
+          Mapper = id }
